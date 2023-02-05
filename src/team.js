67c5d03a2e1409a7d1a1eb7863948606c9685da1
Ravneet Kaur Sammewali <ravneet.sammewali01@gmail.com>
2023-02-05 07:45:10 -0500

    Added html file

--- src/team.js
+++ src/team.js
@@ -29,6 +29,7 @@ const renderteam = (team) => {
             </div>
             </div>
             </div>`;
+    
 
     const renderIntern = (intern) => `
         <div class = "employee-card ">
@@ -45,7 +46,7 @@ const renderteam = (team) => {
             </div>
             </div>`;
 
-    const html = [];
+            const html = [];
 
     html.push(team.filter(employee => employee.getRole() === "Manager").map(manager => renderManager(manager)));
 
@@ -65,6 +66,7 @@ const renderMain = (team) => `
     <meta charset = "utf-8">
     <meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Team</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel = "stylesheet" href = "./style.css">
 
 </head>
